5005ea294f344f8432ba7e5dd05ffe97aab9c161
Ben Kurose <ben.kurose@gmail.com>
2022-08-12 14:03:49 -0400

    commit tree 0b1c9894800ca4277c2d8dd06daf3d4098e409c8

--- index.js
+++ index.js
@@ -1,1 +1,28 @@
-// Add your code here
+// submitData Function takes two arguments: name and email
+function submitData(userName, userEmail){
+    //fetch method, connecting to the /users server and providing data in object form
+    return fetch('http://localhost:3000/users', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+            'Accept': 'application/json',
+        },
+        body: JSON.stringify({
+            //using a JSON.stringify method to turn the follow object in the a JSON string
+            name: `${userName}`,
+            email: `${userEmail}`,
+            }
+        ),
+    })
+    .then(res => res.json())
+    //takes the returned JSON and converts it back to a JS Object
+    .then(res => {
+        const DOMtarget = document.querySelector('body')
+        //selecting the body part of the DOM
+        DOMtarget.textContent = `${res.id}`
+        //logging the ID of newly created nested object on server, onto the body of the DOM
+    })
+    .catch(error => document.querySelector('body').textContent = `${error.message}`);
+}
+
+submitData();
